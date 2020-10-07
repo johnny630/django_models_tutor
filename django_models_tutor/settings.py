69b503aedb8deab2524ee74706096f2b524fbf85
johnnyliu <bakolama@gmail.com>
2020-10-07 22:04:48 +0800

    Initial postgresql set and start mypp
    `python manage.py startapp myapp`

--- django_models_tutor/settings.py
+++ django_models_tutor/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_models_tutor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_models_tutor',
+        'USER': 'postgres',
+        'PASSWORD': 'Passw0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
