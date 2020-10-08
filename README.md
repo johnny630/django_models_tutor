852bc8f6f43766934e07aa18d2e58c77c815914f
johnnyliu <bakolama@gmail.com>
2020-10-08 21:37:47 +0800

    Add columns to Person table
    https://docs.djangoproject.com/zh-hans/3.1/ref/models/fields/#model-field-types

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Django 3.1.x
 `python manage.py migrate` really migrate 
 `python manage.py makemigrations APP_NAME` make migration sql
 `python manage.py sqlmigrate APP_NAME 0001` see migration sql
+
+
+# Add columns to Person table test field types
+https://docs.djangoproject.com/zh-hans/3.1/ref/models/fields/#model-field-types
--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,23 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    age = models.IntegerField(null=True)
+    momey = models.BigIntegerField(null=True)
+    data = models.BinaryField(null=True)
+    has_child = models.BooleanField(default=False)
+    birthdoay = models.DateField(null=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    period_at = models.DurationField(null=True)
+    email = models.EmailField(null=True)
+    ip = models.GenericIPAddressField(null=True)
+    info = models.JSONField(null=True)  # New in Django 3.1.
+    description = models.TextField(null=True)
+    website_url = models.URLField(null=True)
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
